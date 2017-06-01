7e4501cf5e8d23ae9d0c1b58bc8f7bef1f79ce47
pavel <FlinForm@gmail.com>
2017-06-01 23:21:58 +0300

    First commit: Task completed

--- app/src/main/java/com/epam/androidlab/androidtrainingtask10/MainActivity.java
+++ app/src/main/java/com/epam/androidlab/androidtrainingtask10/MainActivity.java
@@ -1,5 +1,6 @@
 package com.epam.androidlab.androidtrainingtask10;
 
+import android.database.Cursor;
 import android.provider.ContactsContract;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
@@ -8,48 +9,55 @@ import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks {
-    private final String[] PROJECTION = {
-                    ContactsContract.Contacts._ID,
-                    ContactsContract.Contacts.LOOKUP_KEY,
-                    ContactsContract.Contacts.DISPLAY_NAME_PRIMARY};
+    static final String[] PROJECTION = new String[] {
+            ContactsContract.Contacts._ID,
+            ContactsContract.Contacts.DISPLAY_NAME,
+            ContactsContract.Contacts.CONTACT_STATUS,
+            ContactsContract.Contacts.CONTACT_PRESENCE,
+            ContactsContract.Contacts.PHOTO_ID,
+            ContactsContract.Contacts.LOOKUP_KEY};
     private SimpleCursorAdapter cursorAdapter;
-    private final static String[] FROM_COLUMNS = {ContactsContract.Contacts.DISPLAY_NAME_PRIMARY};
-    private final static int[] TO_IDS = {android.R.id.text1};
+    private ListView contactList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView contactList = (ListView) findViewById(R.id.list);
+        contactList = (ListView) findViewById(R.id.list);
+
         cursorAdapter = new SimpleCursorAdapter(
                 this,
-                R.layout.list_item,
+                R.id.layout,
                 null,
-                FROM_COLUMNS, TO_IDS,
+                new String[] {
+                        ContactsContract.Contacts.DISPLAY_NAME,
+                        ContactsContract.Contacts.CONTACT_STATUS},
+                new int[] {android.R.id.text1, android.R.id.text2},
                 0);
-        contactList.setAdapter(cursorAdapter);
 
-    getSupportLoaderManager().initLoader(0, null, this);
+        getSupportLoaderManager().initLoader(0, null, this);
     }
 
     @Override
     public Loader onCreateLoader(int id, Bundle args) {
-        String SELECTION = ContactsContract.Contacts.DISPLAY_NAME_PRIMARY + " LIKE ?";
-        return new CursorLoader(
-                this,
+        String select = "((" + ContactsContract.Contacts.DISPLAY_NAME + " NOTNULL) AND ("
+                + ContactsContract.Contacts.HAS_PHONE_NUMBER + "=1) AND ("
+                + ContactsContract.Contacts.DISPLAY_NAME + " != '' ))";
+        return new CursorLoader(this,
                 ContactsContract.Contacts.CONTENT_URI,
                 PROJECTION,
-                SELECTION,
+                select,
                 null,
-                null);
+                ContactsContract.Contacts.DISPLAY_NAME + " COLLATE LOCALIZED ASC");
     }
 
     @Override
     public void onLoadFinished(Loader loader, Object data) {
-        cursorAdapter.swapCursor(null);
+        cursorAdapter.swapCursor((Cursor) data);
     }
 
     @Override
